18f2956609d646862c81e9e5a61bf0f410388da5
Cornell Horner <novadude12@gmail.com>
2018-11-17 17:30:32 +0000

    LInked pages.

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,32 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description = "Make the curriculum"
-    @todo_pomodoro_estimate = 4
-
+        todo_id = params[:id]
+        if todo_id == "1"
+            @todo_description = "Get a job"
+        @todo_pomodoro_estimate = 1
+        elsif todo_id == "2"
+         @todo_description = "Study　日本語 for a couple hours"
+         @todo_pomodoro_estimate = 4
+        elsif todo_id == "3"
+         @todo_description = "Get enough sleep　おやすみ"
+         @todo_pomodoro_estimate = 1    
+        elsif todo_id =="4"
+        @todo_description = "道の"
+         @todo_pomodoro_estimate = 1 
+          elsif todo_id =="5"
+        @todo_description = "道の"
+         @todo_pomodoro_estimate = 1 
+          elsif todo_id =="6"
+        @todo_description = "道の"
+         @todo_pomodoro_estimate = 1 
+          elsif todo_id =="7"
+        @todo_description = "道の"
+         @todo_pomodoro_estimate = 1 
+          elsif todo_id =="8"
+        @todo_description = "道の"
+         @todo_pomodoro_estimate = 1 
+        end
+       
     end
 end
