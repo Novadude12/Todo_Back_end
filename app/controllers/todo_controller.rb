ade5a2674b1035262df2aa6e08e38056470021d7
Cornell Horner <novadude12@gmail.com>
2018-11-18 15:46:09 +0000

    Updated

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,14 +4,24 @@ class TodoController < ApplicationController
     def show
    @todo=Todo.find_by_id(params[:id])
     end  
-    
-    def new
-        def create
+    def edit
+        @todo=Todo.find_by_id(params[:id])
+    end
+    def update
+        t=Todo.find_by_id(params[:id])
+            t.description = params['description']
+            t.pomodoro_estimate = params['pomodoro_estimate']
+            t.save 
+            redirect_to "/todo/show/#{t.id }"
+    end
+    def create
             t=Todo.new
             t.description = params['description']
             t.pomodoro_estimate = params['pomodoro_estimate']
             t.save
             redirect_to "/todo/show/#{ t.id }"
-        end
+    end
+    def new
+       
     end
 end
